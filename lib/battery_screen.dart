f03c96d9f5e1c98c7f2979940013cb9ab130fbaf
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-11-14 01:39:38 +0200

    add native code

--- lib/battery_screen.dart
+++ lib/battery_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class BatteryScreen extends StatelessWidget {
+class BatteryScreen extends StatefulWidget {
   const BatteryScreen({super.key});
 
   @override
+  State<BatteryScreen> createState() => _BatteryScreenState();
+}
+
+class _BatteryScreenState extends State<BatteryScreen> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+
+  // Get battery level.
+String _batteryLevel = 'Unknown battery level.';
+
+Future<void> _getBatteryLevel() async {
+  String batteryLevel;
+  try {
+    final result = await platform.invokeMethod<int>('getBatteryLevel');
+    batteryLevel = 'Battery level at $result % .';
+  } on PlatformException catch (e) {
+    batteryLevel = "Failed to get battery level: '${e.message}'.";
+  }
+
+  setState(() {
+    _batteryLevel = batteryLevel;
+  });
+}
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Get Phone Battery"),
+        centerTitle: true,
+      ),
+      body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: _getBatteryLevel,
+            child: const Text('Get Battery Level'),
+          ),
+          Text(_batteryLevel),
+        ],
+      ),
+    ),
+    );
   }
 }
